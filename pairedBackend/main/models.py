9b49c60dff37de29417adf764d56a7564d2831a1
souhard <souhardswami987@gmail.com>
2020-09-20 13:46:07 +0530

    pics added

--- pairedBackend/main/models.py
+++ pairedBackend/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class Pic(models.Model):
+
+    name=models.CharField(max_length=20,null=False)
+
+    photo = models.ImageField(upload_to="gallery")
+
+    def __str__(self):
+        return self.name
--- pairedBackend/main/views.py
+++ pairedBackend/main/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 
 from django.http import request,HttpResponse
+
+from .models import Pic
 # Create your views here.
 
 
 def home(request):
     print("hello world")
+    model=Pic.objects.all()
+   
+    context={
+        "gif":model
+        }
 
-    return render(request,'index.html')
+    return render(request,'index.html',context)
